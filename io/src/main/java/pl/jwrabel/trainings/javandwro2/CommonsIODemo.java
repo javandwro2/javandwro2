0b34170d54358e8ed8bb03aa0acaf860548900c7
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-03-11 13:14:09 +0100

    update

---
+++ io/src/main/java/pl/jwrabel/trainings/javandwro2/CommonsIODemo.java
@@ -0,0 +1,33 @@
+package pl.jwrabel.trainings.javandwro2;
+
+import com.google.common.base.Charsets;
+import org.apache.commons.io.FileSystemUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.comparator.LastModifiedFileComparator;
+import org.apache.commons.io.comparator.SizeFileComparator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by jakubwrabel on 11.03.2017.
+ */
+public class CommonsIODemo {
+	public static void main(String[] args) throws IOException {
+		// Przeczytanie wszystkich linii z pliku
+		List<String> stringList = FileUtils.readLines(new File("test.txt"), Charsets.UTF_8);
+
+		// Sortowanie po rozmiarze
+		SizeFileComparator sizeFileComparator = new SizeFileComparator();
+		File[] files = {new File("a.txt"), new File("b.txt")};
+		sizeFileComparator.sort(files);
+
+//		sizeFileComparator.compare()
+
+
+		long freeSpaceKb = FileSystemUtils.freeSpaceKb("c:\\");
+
+		FileUtils.cleanDirectory(new File("myDir"));
+	}
+}
