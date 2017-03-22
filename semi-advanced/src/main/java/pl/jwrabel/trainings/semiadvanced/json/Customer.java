f820f0527e5d2a27fc3376ec103655b3b8063bce
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-03-22 19:29:33 +0100

    update

---
+++ semi-advanced/src/main/java/pl/jwrabel/trainings/semiadvanced/json/Customer.java
@@ -0,0 +1,10 @@
+package pl.jwrabel.trainings.semiadvanced.json;
+
+/**
+ * Created by jakubwrabel on 22.03.2017.
+ */
+public class Customer {
+	String firstName;
+	String lastName;
+	String id;
+}
---
+++ semi-advanced/src/main/java/pl/jwrabel/trainings/semiadvanced/json/Place.java
@@ -0,0 +1,45 @@
+package pl.jwrabel.trainings.semiadvanced.json;
+
+import pl.jwrabel.trainings.semiadvanced.comparing.Point;
+
+import java.util.List;
+
+/**
+ * Created by jakubwrabel on 22.03.2017.
+ */
+public class Place {
+	String name;
+	String city;
+
+	List<Point> points;
+
+	public Place(String name, String city, List<Point> points) {
+		this.name = name;
+		this.city = city;
+		this.points = points;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public List<Point> getPoints() {
+		return points;
+	}
+
+	public void setPoints(List<Point> points) {
+		this.points = points;
+	}
+}
