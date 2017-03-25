7e5f50bc6e56b163293d3e1ce466fbabaf4dd515
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-03-25 12:02:37 +0100

    update

--- semi-advanced/src/main/java/pl/jwrabel/trainings/semiadvanced/rest/UnirestTest.java
+++ semi-advanced/src/main/java/pl/jwrabel/trainings/semiadvanced/rest/UnirestTest.java
@@ -106,7 +106,7 @@ public class UnirestTest {
 		System.out.println(postResponse);
 
 		JsonNode weatherJson = Unirest.get("http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=a1fb2306e8575f67c23fc8f23062f7e1").asJson().getBody();
-		double windSpeed = weatherJson.getObject().optJSONObject("wind").getDouble("speed")
+		double windSpeed = weatherJson.getObject().optJSONObject("wind").getDouble("speed");
 
 		System.out.println(windSpeed);
 
