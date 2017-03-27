280509510f36ab2e517cf46c09419e698914dcc3
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-03-27 19:38:45 +0200

    update

---
+++ semi-advanced/src/main/java/pl/jwrabel/trainings/semiadvanced/lombok/Person.java
@@ -0,0 +1,19 @@
+package pl.jwrabel.trainings.semiadvanced.lombok;
+
+/**
+ * Created by jakubwrabel on 27.03.2017.
+ */
+
+//@Getter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Equals
+//@Value
+public class Person {
+	private String firstName;
+	private String lastName;
+	private String id;
+	private String birthYear;
+	private double weight;
+
+}
